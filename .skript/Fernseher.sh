63da6a5ac9a5fb0a4726f2f12eacaabdbbf83039
alex <tetrisiq@web.de>
2017-11-28 11:30:35 +0100

    gitignore

--- .skript/Fernseher.sh
+++ .skript/Fernseher.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+bspc monitor HDMI1 -d 8
 xrandr --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 polybar -c ~/.config/polybar/themes/darkpx/polybar top2 -q > /dev/null &
